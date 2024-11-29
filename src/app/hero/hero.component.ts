e88f749d2de5c2c1afdf3248be7320cf7f17f7ed
Cenk Korkmaz <cenk.korkmaz92@gmail.com>
2024-11-29 14:52:09 +0100

    added hover for nav bar and social icons

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
+  selectedLanguage: string = 'EN'; // Default language
+
+  // Function to set the language
+  setLanguage(language: string) {
+    this.selectedLanguage = language;
+  }
 }
