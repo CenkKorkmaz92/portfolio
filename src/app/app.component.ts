5efebb9baf433e778a82a6e14e985e6f22a31a4c
Cenk Korkmaz <cenk.korkmaz92@gmail.com>
2024-12-02 12:14:49 +0100

    added about me component, linked it, added img and icons and text, multible containers and scss andhtml

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+
 
 export const routes: Routes = [
-  { path: '', component: HeroComponent }, // Default route
-  { path: '**', redirectTo: '' } // Wildcard route for undefined paths
+  { path: '', component: HeroComponent },       // Default route
+  { path: 'about', component: AboutComponent }, // Route for AboutComponent
+  { path: '**', redirectTo: '' }                // Wildcard route for undefined paths
 ];
+
