b6c600464a36067609357f5b2be615da4664cf66
Cenk Korkmaz <cenk.korkmaz92@gmail.com>
2024-12-05 09:52:48 +0100

    added featured projects component, html and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeroComponent, AboutComponent, SkillsComponent],
+  imports: [CommonModule, HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },       // Default route
   { path: 'about', component: AboutComponent }, // Route for AboutComponent
   { path: 'skills', component: SkillsComponent }, // Route for SkillsComponent
+  { path: 'skills', component: PortfolioComponent}, // Route for PortfolioComponent
   { path: '**', redirectTo: '' }                // Wildcard route for undefined paths
 ];
 
