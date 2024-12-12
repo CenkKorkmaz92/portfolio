dcd880d51afbc8617368be3080872992cc93a490
Cenk Korkmaz <cenk.korkmaz92@gmail.com>
2024-12-12 11:00:14 +0100

    more scss and html

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
-
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
