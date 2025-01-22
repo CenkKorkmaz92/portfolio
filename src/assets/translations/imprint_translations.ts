b8033daf3fcaf3eabe034d7f20cb45229f136740
Cenk Korkmaz <cenk.korkmaz92@gmail.com>
2025-01-22 12:20:39 +0100

    added translations for legal notice and imprint

--- src/assets/translations/imprint_translations.ts
+++ src/assets/translations/imprint_translations.ts
@@ -1,29 +1,80 @@
 export const TRANSLATIONS = {
     en: {
-        WHO_I_AM: 'Who I Am',
-        ABOUT_ME: 'About Me',
-        DESCRIPTION: `Hi, I’m a German-speaking Frontend Developer based in Sulzbach an der Murr. 
-        Motivated by the limitless opportunities within IT, I am excited about crafting visually 
-        captivating and intuitive websites and applications.`,
-        LOCATION: `Highly focused in terms of working, I can work effectively both remotely and on site.`,
-        MIND_SET: `I am open-minded and always looking for personal challenges to constantly 
-        improve my knowledge and skills.`,
-        PRO: `In my profession, programming isn't just about writing code; 
-        it's a creative form of problem-solving. I take pride in my ability to distill complex 
-        technical challenges into simple, user-friendly solutions, helping you achieve your goals.`,
+        heading1: 'Imprint',
+        heading2: 'Information According to § 5 TMG',
+        block2: `
+          <p>
+            Philipp Schönborn<br>
+            Baumschulenring 26<br>
+            72202 Nagold
+          </p>
+        `,
+        heading3: 'Contact',
+        block3: `
+          <p>
+            Phone: +49 (0) 176 306 785 30<br>
+            Email:
+            <a href="mailto:philipp@schoenborn-home.de">
+              philipp&#64;schoenborn-home.de
+            </a>
+          </p>
+        `,
+        heading4: 'Editorially Responsible',
+        block4: `
+          <p>
+            Philipp Schönborn<br>
+            Baumschulenring 26<br>
+            72202 Nagold
+          </p>
+        `,
+        block5: `
+          <p>
+            Source:
+            <a href="https://www.e-recht24.de"
+               target="_blank"
+               rel="noopener nofollow">
+              https://www.e-recht24.de
+            </a>
+          </p>
+        `
     },
     de: {
-        WHO_I_AM: 'Wer ich bin',
-        ABOUT_ME: 'Über mich',
-        DESCRIPTION: `Hallo, ich bin ein deutschsprachiger Frontend-Entwickler aus Sulzbach an der Murr. 
-        Durch die unbegrenzten Möglichkeiten in der IT bin ich motiviert, 
-        visuell ansprechende und intuitive Webseiten und Anwendungen zu gestalten.`,
-        LOCATION: `Ich arbeite sehr fokussiert und kann sowohl remote als auch vor Ort effektiv arbeiten.`,
-        MIND_SET: `Ich bin offen für Neues und suche ständig nach persönlichen Herausforderungen, 
-        um mein Wissen und meine Fähigkeiten weiterzuentwickeln.`,
-        PRO: `In meinem Beruf geht es beim Programmieren nicht nur ums reine Coden, 
-        sondern um kreative Problemlösung. Ich lege großen Wert darauf, 
-        komplexe technische Herausforderungen in einfache, benutzerfreundliche Lösungen zu verwandeln, 
-        damit du deine Ziele erreichst.`,
+        heading1: 'Impressum',
+        heading2: 'Angaben gemäß § 5 TMG',
+        block2: `
+          <p>
+            Philipp Schönborn<br>
+            Baumschulenring 26<br>
+            72202 Nagold
+          </p>
+        `,
+        heading3: 'Kontakt',
+        block3: `
+          <p>
+            Telefon: +49 (0) 176 306 785 30<br>
+            E-Mail:
+            <a href="mailto:philipp@schoenborn-home.de">
+              philipp&#64;schoenborn-home.de
+            </a>
+          </p>
+        `,
+        heading4: 'Redaktionell verantwortlich',
+        block4: `
+          <p>
+            Philipp Schönborn<br>
+            Baumschulenring 26<br>
+            72202 Nagold
+          </p>
+        `,
+        block5: `
+          <p>
+            Quelle:
+            <a href="https://www.e-recht24.de"
+               target="_blank"
+               rel="noopener nofollow">
+              https://www.e-recht24.de
+            </a>
+          </p>
+        `
     }
 };
